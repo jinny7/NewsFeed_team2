dbb9c917d7276725fcdc0e2151a9cf455c5aa6bc
hyungs0703 <hyungs0703@naver.com>
2024-06-05 11:47:39 +0900

    git init

---
+++ src/main/java/com/sparta/newspeed/NewSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSpeedApplication.class, args);
+    }
+
+}
